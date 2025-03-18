e39e4418d6d8ca1b7620073d3ec4c0e00069eeb5
KyleAktr <aktr.contact@gmail.com>
2025-03-18 10:57:48 +0100

    home page style added

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,7 @@
-import Navigation from "@/components/Navigation";
+import Navbar from "@/components/Navbar";
 import type { Metadata } from "next";
+import homePageImage from "../static/img/home-page-image.png";
+import Image from "next/image";
 
 export const metadata: Metadata = {
   title: "Forum - Accueil",
@@ -9,8 +11,26 @@ export const metadata: Metadata = {
 export default function Home() {
   return (
     <div>
-      <h1>Forum</h1>
-      <Navigation />
+      <Navbar />
+
+      <header className="header">
+        {/* Image en arrière-plan */}
+        <Image
+          src={homePageImage}
+          alt="Image"
+          className="header-img"
+          layout="fill"
+          objectFit="cover"
+        />
+
+        {/* Contenu au-dessus de l'image */}
+        <div className="header-content">
+          <h1>Island°</h1>
+          <h2>Faites vivre vos pensées</h2>
+          <p>Exprimez-vous, réagissez, inspirez-vous</p>
+          <button>Commencer</button>
+        </div>
+      </header>
     </div>
   );
 }
--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,16 +1,48 @@
+"use client";
+import { useEffect, useState } from "react";
+import { isAuthenticated, logout, getUser } from "@/services/auth";
+
 export default function Navbar() {
+  const [authenticated, setAuthenticated] = useState(false);
+  const [username, setUsername] = useState("");
+
+  useEffect(() => {
+    const auth = isAuthenticated();
+    setAuthenticated(auth);
+    if (auth) {
+      const user = getUser();
+      if (user) {
+        setUsername(user.username);
+      }
+    }
+  }, []);
+
   return (
     <div className="navbar">
-      <nav>
-        <div className="logo">
-          <a href="/">Island°</a>
-        </div>
-        <div className="ancres">
-          <a href="/categories">Catégories</a>
-          <a href="/articles">Explorer</a>
-          <a href="/contact">Contact</a>
-        </div>
-      </nav>
+      {authenticated ? (
+        <nav>
+          <div className="logo">
+            <a href="/">Island°</a>
+          </div>
+          <div className="ancres">
+            <a href="/categories">Catégories</a>
+            <a href="/articles">Explorer</a>
+            <a href="/contact">Contact</a>
+            <button onClick={logout}>Déconnexion</button>
+          </div>
+        </nav>
+      ) : (
+        <nav>
+          <div className="logo">
+            <a href="/">Island°</a>
+          </div>
+          <div className="ancres">
+            <a href="/contact">Contact</a>
+            <a href="/login">connexion</a>
+            <a href="/register">s'inscrire</a>
+          </div>
+        </nav>
+      )}
     </div>
   );
 }
