22ec73b226159a75180c8ae3680a9c6f02daab3e
KyleAktr <aktr.contact@gmail.com>
2025-03-20 10:07:11 +0100

    front categories created

--- frontend/src/app/categories/page.tsx
+++ frontend/src/app/categories/page.tsx
@@ -4,37 +4,50 @@ import minimalisme from "../../static/img/minimalisme.png";
 import santeDigitale from "../../static/img/sante-digitale.png";
 import teletravail from "../../static/img/teletravail.png";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function categories() {
   return (
     <div className="categories">
       <Navbar />
       <div className="categories-cards">
-        <div className="categorie-card">
-          <Image
-            src={teletravail}
-            alt="Image"
-            className="categorie-card-image"
-          />
-        </div>
-        <div className="categorie-card">
-          <Image
-            src={minimalisme}
-            className="categorie-card-image"
-            alt="Image"
-          />
-          <p className="categorie-text">minimalisme</p>
-        </div>
-        <div className="categorie-card">
-          <Image
-            src={santeDigitale}
-            alt="Image"
-            className="categorie-card-image"
-          />
-        </div>
-        <div className="categorie-card">
-          <Image src={lifestyle} alt="Image" className="categorie-card-image" />
-        </div>
+        <Link href="/categories/teletravail">
+          <div className="categorie-card">
+            <Image
+              src={teletravail}
+              alt="Image"
+              className="categorie-card-image"
+            />
+          </div>
+        </Link>
+        <Link href="/categories/minimalisme">
+          <div className="categorie-card">
+            <Image
+              src={minimalisme}
+              className="categorie-card-image"
+              alt="Image"
+            />
+            <p className="categorie-text">minimalisme</p>
+          </div>
+        </Link>
+        <Link href="/categories/santeDigitale">
+          <div className="categorie-card">
+            <Image
+              src={santeDigitale}
+              alt="Image"
+              className="categorie-card-image"
+            />
+          </div>
+        </Link>
+        <Link href="/categories/lifestyle">
+          <div className="categorie-card">
+            <Image
+              src={lifestyle}
+              alt="Image"
+              className="categorie-card-image"
+            />
+          </div>
+        </Link>
       </div>
     </div>
   );
---
+++ frontend/src/app/categories/santeDigitale/page.tsx
@@ -0,0 +1,10 @@
+import Navbar from "@/components/Navbar";
+
+export default function page() {
+  return (
+    <div>
+      <Navbar />
+      <h1>Santé digitale</h1>
+    </div>
+  );
+}
