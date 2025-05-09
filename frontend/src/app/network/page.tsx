4ed0110ecac2699fbc5e302edc64403fe5509497
PAYEN Camille <camillepayen35@gmail.com>
2025-05-09 23:03:41 +0200

    Added user search network section

--- frontend/src/app/network/page.tsx
+++ frontend/src/app/network/page.tsx
@@ -2,13 +2,15 @@
 
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
+import { User } from "@/types";
 import Image from "next/image";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 
-export default function page() {
-  const [users, setUsers] = useState([]);
-
+export default function Page() {
+  const [users, setUsers] = useState<User[]>([]);
+  const [searchInput, setSearchInput] = useState("");
+  
   useEffect(() => {
     const fetchUsers = async () => {
       const res = await fetch(`http://localhost:8080/api/users`);
@@ -18,18 +20,31 @@ export default function page() {
     fetchUsers();
   }, []);
 
+  const filteredUsers = users.filter(user => 
+    user.username.toLowerCase().includes(searchInput.toLowerCase())
+  );
+
   return (
     <div className="network">
       <Navbar />
+      
+      <div className="search-user">
+        <input
+          type="text"
+          placeholder="Rechercher un utilisateur"
+          value={searchInput}
+          onChange={(e) => setSearchInput(e.target.value)}
+        />
+      </div>
 
       <div className="network-list">
-        {users.map((user: any) => (
+        {filteredUsers.map((user) => (
           <div key={user.id} className="network-card">
             <Image
               src={
-                user.profilePicture.startsWith("http")
+                user.profilePicture?.startsWith("http")
                   ? user.profilePicture
-                  : `http://localhost:8080${user.profilePicture}`
+                  : `http://localhost:8080${user.profilePicture || ""}`
               }
               alt="Photo de profil"
               width={150}
