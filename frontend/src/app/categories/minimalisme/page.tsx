a5d0dececc56c9f913736b557641ac26232b45e5
KyleAktr <aktr.contact@gmail.com>
2025-05-13 11:33:47 +0200

    responsive part2

--- frontend/src/app/categories/minimalisme/page.tsx
+++ frontend/src/app/categories/minimalisme/page.tsx
@@ -16,7 +16,7 @@ export default function page() {
           alt="Image représentant une femme dans l'eau"
           className="header-categorie-img"
         />
-        <h1>MINIMALISME ET FRAUGALITÉ</h1>
+        <h1>MINIMALISME ET FRUGALITÉ</h1>
       </header>
       <CategorieCard categoryId={2} />
       <Footer />
